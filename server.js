5510187bf5d1b9be505f36e8f848e2427c00dd44
DarVoid <jorgemduque@hotmail.com>
2022-05-15 17:51:53 +0100

    [change] Error handling

--- server.js
+++ server.js
@@ -7,58 +7,87 @@ const port = 8000;
 const requestListener = function (req, res) {
     const url = req.url.split("/");
     const method = req.method;
-    console.log(url)
+    console.log(url);
     switch (method) {
         case "GET":
         case "PUT":
         case "DELETE":
         case "POST":
-            if(url[1] === "create") {
-
+            if (url[1] === "create") {
                 client.CreateGame(
                     {
-                        winChance: 20, 
+                        winChance: 20,
                         totalJogadas: 200,
                     },
                     (error, game) => {
-                        if (error) throw error;
-                        res.write(JSON.stringify({
-                            data: game,
-                            msg: "Successfully created a game.",
-                        }));
-                        res.end();
+                        if (error) {
+                            console.error(error);
+                            JSON.stringify({
+                                msg: "Could not create game",
+                            })
+                            res.end();
+                        } else {
+                            res.write(
+                                JSON.stringify({
+                                    data: game,
+                                    msg: "Successfully created a game.",
+                                })
+                            );
+                            res.end();
+                        }
                     }
-                    );
-            }else if(url[1] === "play"){
+                );
+            } else if (url[1] === "play") {
                 client.PlayGame(
                     {
                         gameId: url[2],
                         name: "Jorge",
-                        luckyQuote: "socorro, let me win"
+                        luckyQuote: "socorro, let me win",
                     },
                     (error, response) => {
-                        if (error) throw error;
-                        res.write(JSON.stringify({
-                            data: response,
-                            msg: "Successfully played a game.",
-                        }));
-                        res.end();
+                        if (error) {
+                            console.error(error);
+                            res.write(JSON.stringify({
+                                msg: "Could not play game",
+                            })
+                            );
+                            res.end();
+                        } else {
+                            res.write(
+                                JSON.stringify({
+                                    data: response,
+                                    msg: "Successfully played a game.",
+                                })
+                            );
+                            res.end();
+                        }
                     }
-                    );
-            }else if(url[1] === "exists"){
+                );
+            } else if (url[1] === "exists") {
                 client.GameExists(
                     {
                         gameId: url[2],
                     },
                     (error, response) => {
-                        if (error) throw error;
-                        res.write(JSON.stringify({
-                            data: response,
-                            msg: "Successfully checked if a game exists.",
-                        }));
-                        res.end();
+                        if (error) {
+                            console.error(error);
+                            res.write(
+                                JSON.stringify({
+                                    msg: "Could not check if game exists",
+                                })
+                            );
+                            res.end();
+                        } else {
+                            res.write(
+                                JSON.stringify({
+                                    data: response,
+                                    msg: "Successfully checked if a game exists.",
+                                })
+                            );
+                            res.end();
+                        }
                     }
-                    );
+                );
             }
             break;
         default:
