70e14c6737abe9f6ce882a62ac60aef7a8370aaa
DarVoid <jorgemduque@hotmail.com>
2022-05-21 16:38:40 +0100

    [change] go mod

--- chat/chat.go
+++ chat/chat.go
@@ -1,4 +1,18 @@
 package chat
 
+import "fmt"
+
 type Server struct {
+}
+
+func (s *Server) SendAndReceiveMessages(obs ChatService_SendAndReceiveMessagesServer) error {
+	a := make(chan *MessageRequest)
+	b := make(chan error)
+
+	go func() {
+		for {
+			a, b := <-obs.Recv(), <-obs.Recv()
+			fmt.Printf("%v\n%v\n", a, b)
+		}
+	}()
 }
